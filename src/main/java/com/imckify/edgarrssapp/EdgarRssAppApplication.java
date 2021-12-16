28cae34943cb6275593d8dabed4e3ca5b9614353
imckify <rokas.mockevicius@gmail.com>
2021-12-16 12:31:58 +0200

    v2 IntegrationFlows.from("myFeedChannel")

--- src/main/java/com/imckify/edgarrssapp/EdgarRssAppApplication.java
+++ src/main/java/com/imckify/edgarrssapp/EdgarRssAppApplication.java
@@ -1,38 +1,25 @@
 package com.imckify.edgarrssapp;
 
 import com.rometools.rome.feed.synd.SyndEntry;
-import com.rometools.rome.feed.synd.SyndFeed;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.feed.dsl.Feed;
 import org.springframework.integration.feed.inbound.FeedEntryMessageSource;
 import org.springframework.integration.metadata.MetadataStore;
 import org.springframework.integration.metadata.PropertiesPersistingMetadataStore;
 import org.springframework.integration.scheduling.PollerMetadata;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.MessagingException;
 import org.springframework.scheduling.support.PeriodicTrigger;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
+
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.List;
+import java.util.Date;
 
 @SpringBootApplication
 public class EdgarRssAppApplication {
@@ -44,14 +31,14 @@ public class EdgarRssAppApplication {
     String url = "https://www.sec.gov/Archives/edgar/xbrlrss.all.xml";  // delete metadata-store.properties if no output
 
     @Bean
-    @InboundChannelAdapter("feedChannel")
+    @InboundChannelAdapter("myFeedChannel")
     FeedEntryMessageSource feedEntrySource() throws MalformedURLException {
         FeedEntryMessageSource source = new FeedEntryMessageSource(new URL(url), "myKey");
         return source;
     }
 
     @Bean
-    public DirectChannel feedChannel() {
+    public DirectChannel myFeedChannel() {
         return new DirectChannel();
     }
 
@@ -62,21 +49,6 @@ public class EdgarRssAppApplication {
         return metadataStore;
     }
 
-    @Bean
-    public IntegrationFlow feedFlow() throws MalformedURLException {
-        return IntegrationFlows
-                .from(Feed.inboundAdapter(new URL(url), "myKey").metadataStore(metadataStore()),
-                        e -> e.poller(p -> p.fixedRate(10000)))
-                .handle(message -> {
-                    SyndEntry entry = (SyndEntry) message.getPayload();
-                    String epoch = entry.getPublishedDate() != null ? String.valueOf(entry.getPublishedDate().toInstant().toEpochMilli()) : "";
-                    String description = entry.getDescription() != null ? entry.getDescription().getValue() : "";
-                    String title = entry.getTitle();
-                    System.out.println(title);
-                }) // or .channel(c -> c.queue("entries"))
-                .get();
-    }
-
     @Bean(name = PollerMetadata.DEFAULT_POLLER)
     public PollerMetadata defaultPoller() {
 
@@ -84,18 +56,40 @@ public class EdgarRssAppApplication {
         pollerMetadata.setTrigger(new PeriodicTrigger(60000));
         return pollerMetadata;
     }
-//    @Bean
+
+    @Bean
+    public IntegrationFlow feedFlow() throws MalformedURLException {
+        return IntegrationFlows
+                //v2
+                .from("myFeedChannel")
+                // v1
+//                .from(  // this exact from() caches using
+//                        Feed.inboundAdapter(new URL(url), "myKey")
+//                        // .metadataStore(metadataStore()) // can be omitted in this exact usage of from()
+//                        , e -> e.poller(Pollers.fixedDelay(1))    // overrides defaultPoller, polls each entry
+//                )
+                .handle(message -> {
+                    String msgDate = new Date(message.getHeaders().getTimestamp()).toString();
+                    SyndEntry entry = (SyndEntry) message.getPayload();
+                    String epoch = entry.getPublishedDate() != null ? String.valueOf(entry.getPublishedDate().toInstant().toEpochMilli()) : "";
+                    String description = entry.getDescription() != null ? entry.getDescription().getValue() : "";
+                    String title = entry.getTitle();
+                    System.out.println("At " + msgDate + " received " + title);
+                }) // or .channel(c -> c.queue("entries"))
+                .get();
+    }
+
 //    public Message handleMessage(Message message) {
-//            Object o = message.getPayload();
-//            List entries = (List) o;
-//            for (int i = 0; i < 5; i++) {
-//                System.out.println(entries.get(i));
-//            }
-//            System.out.println("==============================================");
+//        String msgDate = new Date(message.getHeaders().getTimestamp()).toString();
+//        SyndEntry entry = (SyndEntry) message.getPayload();
+//        String epoch = entry.getPublishedDate() != null ? String.valueOf(entry.getPublishedDate().toInstant().toEpochMilli()) : "";
+//        String description = entry.getDescription() != null ? entry.getDescription().getValue() : "";
+//        String title = entry.getTitle();
+//        System.out.println("At " + msgDate + " received " + title);
+//
 //        return null;
 //    }
 
-//    Todo debug each method in cloned (other) project
 //    @Bean
 //    public Message<List<SyndFeed>> handle() {
 //        List<SyndFeed> syndFeeds = syndFeedMessage.getPayload();
