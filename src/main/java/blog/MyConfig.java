7d4ed6710b213ab10da3af5d5ba81ab0912f27e3
imckify <rokas.mockevicius@gmail.com>
2021-12-19 17:11:16 +0200

    transform, @Poller, handle

--- src/main/java/blog/MyConfig.java
+++ src/main/java/blog/MyConfig.java
@@ -1,37 +1,30 @@
 package blog;
 
 import blog.helper.FetcherEventListenerImpl;
-import org.slf4j.Logger;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.scheduling.PollerMetadata;
-import org.springframework.scheduling.support.PeriodicTrigger;
+import org.springframework.integration.transformer.AbstractPayloadTransformer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableIntegration
 public class MyConfig {
-    private static Logger logger = LoggerFactory.getLogger(MyConfig.class);
-
-    @Bean(name = PollerMetadata.DEFAULT_POLLER)
-    public PollerMetadata defaultPoller() {
-        PollerMetadata pollerMetadata = new PollerMetadata();
-        PeriodicTrigger trigger = new PeriodicTrigger(5000);
-        trigger.setFixedRate(true);
-        pollerMetadata.setTrigger(trigger);
-        pollerMetadata.setMaxMessagesPerPoll(1);
-        return pollerMetadata;
-    }
 
     @Bean
-    @InboundChannelAdapter(value = "newsInput")
+    @InboundChannelAdapter(value = "newsInput", poller = @Poller(fixedRate = "5000", maxMessagesPerPoll = "1"))
     public NewsReader newsReader() {
         List<String> urls = Arrays.asList(
                 "https://www.sec.gov/Archives/edgar/xbrlrss.all.xml",
@@ -46,22 +39,60 @@ public class MyConfig {
     }
 
     @Bean
-    public NewsItemTransformer newsItemTransformer() {
-        return new NewsItemTransformer();
-    }
+    public AbstractPayloadTransformer<List<SyndFeed>, List<NewsItem>> transformMessages() {
+        return new AbstractPayloadTransformer<List<SyndFeed>, List<NewsItem>>() {
+            @Override
+            protected List<NewsItem> transformPayload(List<SyndFeed> syndFeeds) {
+                List<NewsItem> newsItems = new ArrayList<NewsItem>();
+                for (SyndFeed syndFeed : syndFeeds) {
+                    List<SyndEntry> items = syndFeed.getEntries();
 
-    @Bean
-    public NewsMessageHandler newsMessageHandler() {
-        return new NewsMessageHandler();
+                    if (syndFeed.getFeedType() == "rss_2.0") {
+                        for (SyndEntry entry : items) {
+                            String epoch = entry.getPublishedDate() != null ? String.valueOf(entry.getPublishedDate().toInstant().toEpochMilli()) : "";
+                            String description = entry.getDescription() != null ? entry.getDescription().getValue() : "";
+                            String title = entry.getTitle();
+                            newsItems.add(new NewsItem(title, description, epoch));
+                        }
+                    } else if (syndFeed.getFeedType() == "atom_1.0") {
+                        for (SyndEntry entry : items) {
+                            String date = entry.getUpdatedDate() != null ? entry.getUpdatedDate().toString() : "";
+                            String epoch = entry.getUpdatedDate() != null ? String.valueOf(entry.getUpdatedDate().toInstant().toEpochMilli()) : "";
+
+                            String description = entry.getCategories().size() != 0 ? entry.getCategories().get(0).getName() : "";
+                            String title = entry.getTitle().replace(description + " - ", "");
+                            newsItems.add(new NewsItem(title, description, epoch));
+                        }
+                    }
+                }
+
+                List<NewsItem> news = newsItems.stream()
+                        .sorted((n1, n2) -> Long.compare(Long.parseLong(n2.getDate()), Long.parseLong(n1.getDate())))
+                        .distinct()
+                        .map(n -> {
+                            Date date = new Date(Long.parseLong(n.getDate()));
+                            return new NewsItem(n.getTitle(), n.getDescription(), date.toString());
+                        }).collect(Collectors.toList());
+
+                return news;
+            }
+        };
     }
 
     @Bean
     public IntegrationFlow myFlow() {
         return IntegrationFlows
                 .from("newsInput")
-                .transform(newsItemTransformer())
+                .transform(transformMessages())
                 .channel("newsOutput")
-                .handle(newsMessageHandler())
+                .handle(message -> {
+                    List<NewsItem> news = (List<NewsItem>) message.getPayload();
+                    String date = new Date(message.getHeaders().getTimestamp()).toString();
+                    LoggerFactory.getLogger(MyConfig.class).info("At {} received a message with feedid {} and payload:", date, message.getHeaders().get("feedid",String.class));
+                    for (int i = 0; i < 5; i++) {
+                        System.out.println(news.get(i).getTitle());
+                    }
+                })
                 .get();
     }
 }
