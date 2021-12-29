8fec5dbfa1efb8519991c4853dcd2dd0b1151b00
imckify <rokas.mockevicius@gmail.com>
2021-12-29 15:22:55 +0200

    minor fix

--- backend/src/main/java/com/imckify/bakis/Bakis.java
+++ backend/src/main/java/com/imckify/bakis/Bakis.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @EnableScheduling
 @EnableConfigurationProperties
