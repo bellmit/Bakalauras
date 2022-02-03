173b8016cab64d8a10cce70b2f0575eaef19e47f
imckify <rokas.mockevicius@gmail.com>
2022-02-03 13:02:22 +0200

    removed example demo courses, authors

--- backend/src/main/java/com/imckify/bakis/repos/AuthorsRepo.java
+++
@@ -1,8 +0,0 @@
-package com.imckify.bakis.repos;
-
-import com.imckify.bakis.models.Authors;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface AuthorsRepo extends JpaRepository<Authors, String> {
-}
