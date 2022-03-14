f6d0ae16a9427c58001fab6aa5fa48d5891ab013
imckify <rokas.mockevicius@gmail.com>
2022-03-14 22:07:18 +0200

    PriceApiTests. methods for reducing code duplication

--- backend/src/test/java/com/imckify/bakis/PriceApiTests.java
+++ backend/src/test/java/com/imckify/bakis/PriceApiTests.java
@@ -19,10 +19,13 @@ import java.io.IOException;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PriceApiTests {
+
+    static final Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
 
     private static String API_nasdaq = "iSF53xkj57EvMFfaxSCk";
     
@@ -35,8 +38,39 @@ public class PriceApiTests {
 
     @BeforeAll
     public static void setUp() {
-        final Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
         logger.setLevel(Level.INFO);
+    }
+
+    private InputStream get(String url) {
+        InputStream inputStream = null;
+        try {
+            CloseableHttpClient client = HttpClients.createMinimal();
+            HttpUriRequest request = new HttpGet(url);
+            CloseableHttpResponse response = client.execute(request);
+            inputStream = response.getEntity().getContent();
+
+            int code = response.getStatusLine().getStatusCode();
+            assertEquals(200 , code);
+        } catch (Exception e) {
+            logger.error(Arrays.toString(e.getStackTrace()));
+        }
+        return inputStream;
+    }
+
+    private String parse(InputStream inputStream) {
+        String pretty = "";
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Object json = mapper.readValue(inputStream, Object.class);
+
+            DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
+            pp.indentArraysWith( DefaultIndenter.SYSTEM_LINEFEED_INSTANCE );
+
+            pretty = mapper.writer(pp).writeValueAsString(json);
+        } catch (Exception e) {
+            logger.error(Arrays.toString(e.getStackTrace()));
+        }
+        return pretty;
     }
 
     @Test
@@ -57,38 +91,24 @@ public class PriceApiTests {
     public void prettyDefaultNasdaqReal() throws IOException {
         String url = "https://data.nasdaq.com/api/v3/datasets/WIKI/FB/data.json?api_key=" + API_nasdaq;
         
-        CloseableHttpClient client = HttpClients.createMinimal();
-        HttpUriRequest request = new HttpGet(url);
-        CloseableHttpResponse response = client.execute(request);
-        InputStream inputStream = response.getEntity().getContent();
+        InputStream inputStream = get(url);
 
         ObjectMapper mapper = new ObjectMapper();
         Object json = mapper.readValue(inputStream, Object.class);
         String pretty = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
 
         System.out.println(pretty);
-        int code = response.getStatusLine().getStatusCode();
-        assertEquals(200 , code);
     }
 
     @Test
     public void nasdaqPrettyArrayReal() throws IOException {
         String url = "https://data.nasdaq.com/api/v3/datasets/WIKI/FB/data.json?api_key=" + API_nasdaq;
 
-        CloseableHttpClient client = HttpClients.createMinimal();
-        HttpUriRequest request = new HttpGet(url);
-        CloseableHttpResponse response = client.execute(request);
-        InputStream inputStream = response.getEntity().getContent();
+        InputStream inputStream = get(url);
 
-        ObjectMapper mapper = new ObjectMapper();
-        Object json = mapper.readValue(inputStream, Object.class);
-        DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
-        pp.indentArraysWith( DefaultIndenter.SYSTEM_LINEFEED_INSTANCE );
-        String pretty = mapper.writer(pp).writeValueAsString(json);
+        String pretty = parse(inputStream);
 
         System.out.println(pretty);
-        int code = response.getStatusLine().getStatusCode();
-        assertEquals(200 , code);
     }
 
     @Test
@@ -96,99 +116,54 @@ public class PriceApiTests {
         String url = "https://cloud.iexapis.com/stable/stock/aapl/previous?token=" + API_iex;
 //        String url = "https://sandbox.iexapis.com/stable/stock/aapl/quote?token=" + API_iex_demo; // Todo not working
 
-        CloseableHttpClient client = HttpClients.createMinimal();
-        HttpUriRequest request = new HttpGet(url);
-        CloseableHttpResponse response = client.execute(request);
-        InputStream inputStream = response.getEntity().getContent();
+        InputStream inputStream = get(url);
 
-        ObjectMapper mapper = new ObjectMapper();
-        Object json = mapper.readValue(inputStream, Object.class);
-        DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
-        pp.indentArraysWith( DefaultIndenter.SYSTEM_LINEFEED_INSTANCE );
-        String pretty = mapper.writer(pp).writeValueAsString(json);
+        String pretty = parse(inputStream);
 
         System.out.println(pretty);
-        int code = response.getStatusLine().getStatusCode();
-        assertEquals(200 , code);
     }
 
     @Test
     public void iexHistoricalReal() throws IOException {
         String url = "https://cloud.iexapis.com/stable/stock/aapl/chart/H12018?token=" + API_iex;
 
-        CloseableHttpClient client = HttpClients.createMinimal();
-        HttpUriRequest request = new HttpGet(url);
-        CloseableHttpResponse response = client.execute(request);
-        InputStream inputStream = response.getEntity().getContent();
+        InputStream inputStream = get(url);
 
-        ObjectMapper mapper = new ObjectMapper();
-        Object json = mapper.readValue(inputStream, Object.class);
-        DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
-        pp.indentArraysWith( DefaultIndenter.SYSTEM_LINEFEED_INSTANCE );
-        String pretty = mapper.writer(pp).writeValueAsString(json);
+        String pretty = parse(inputStream);
 
         System.out.println(pretty);
-        int code = response.getStatusLine().getStatusCode();
-        assertEquals(200 , code);
     }
 
     @Test
     public void alphaVantageYesterdayDemo() throws IOException {
         String url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=" + API_alphaVantage_demo;
 
-        CloseableHttpClient client = HttpClients.createMinimal();
-        HttpUriRequest request = new HttpGet(url);
-        CloseableHttpResponse response = client.execute(request);
-        InputStream inputStream = response.getEntity().getContent();
+        InputStream inputStream = get(url);
 
-        ObjectMapper mapper = new ObjectMapper();
-        Object json = mapper.readValue(inputStream, Object.class);
-        DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
-        pp.indentArraysWith( DefaultIndenter.SYSTEM_LINEFEED_INSTANCE );
-        String pretty = mapper.writer(pp).writeValueAsString(json);
+        String pretty = parse(inputStream);
 
         System.out.println(pretty);
-        int code = response.getStatusLine().getStatusCode();
-        assertEquals(200 , code);
     }
 
     @Test
     public void alphaVantageHistoricalDemo() throws IOException {
         String url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=" + API_alphaVantage_demo;
 
-        CloseableHttpClient client = HttpClients.createMinimal();
-        HttpUriRequest request = new HttpGet(url);
-        CloseableHttpResponse response = client.execute(request);
-        InputStream inputStream = response.getEntity().getContent();
+        InputStream inputStream = get(url);
 
-        ObjectMapper mapper = new ObjectMapper();
-        Object json = mapper.readValue(inputStream, Object.class);
-        DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
-        pp.indentArraysWith( DefaultIndenter.SYSTEM_LINEFEED_INSTANCE );
-        String pretty = mapper.writer(pp).writeValueAsString(json);
+        String pretty = parse(inputStream);
 
         System.out.println(pretty);
-        int code = response.getStatusLine().getStatusCode();
-        assertEquals(200 , code);
     }
     @Test
     public void alphaVantageHistoricalReal() throws IOException {
         String url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&outputsize=full&apikey=" + API_alphaVantage;
 
-        CloseableHttpClient client = HttpClients.createMinimal();
-        HttpUriRequest request = new HttpGet(url);
-        CloseableHttpResponse response = client.execute(request);
-        InputStream inputStream = response.getEntity().getContent();
+        InputStream inputStream = get(url);
 
-        ObjectMapper mapper = new ObjectMapper();
-        Object json = mapper.readValue(inputStream, Object.class);
-        DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
-        pp.indentArraysWith( DefaultIndenter.SYSTEM_LINEFEED_INSTANCE );
-        String pretty = mapper.writer(pp).writeValueAsString(json);
+        String pretty = parse(inputStream);
 
         System.out.println(pretty);
-        int code = response.getStatusLine().getStatusCode();
-        assertEquals(200 , code);
     }
     
 }
