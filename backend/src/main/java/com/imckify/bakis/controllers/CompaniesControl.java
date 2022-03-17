2e19f5fe86bd8b4fb4ae7470326806e21c9856b6
imckify <rokas.mockevicius@gmail.com>
2022-03-17 16:40:55 +0200

    WatchlistTable initial quotes

--- backend/src/main/java/com/imckify/bakis/controllers/CompaniesControl.java
+++ backend/src/main/java/com/imckify/bakis/controllers/CompaniesControl.java
@@ -1,14 +1,23 @@
 package com.imckify.bakis.controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.imckify.bakis.Bakis.ResourceNotFoundException;
 import com.imckify.bakis.models.Companies;
 import com.imckify.bakis.models.CompaniesVM;
 import com.imckify.bakis.repos.CompaniesRepo;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -56,5 +65,40 @@ public class CompaniesControl {
 //                    return this.CompaniesRepo.save(newCompany);
 //                });
 //    }
+
+    @GetMapping("/yahoo/spark/quotes")
+    public Object getLastPrices(){
+
+        String[] tickers = this.getCompanies().stream().map(CompaniesVM::getTicker).toArray(String[]::new);
+        List<List<String>> chunks = new ArrayList<>();
+
+        int chunkSize = 20; // chunk size to divide
+        for(int i = 0; i < tickers.length; i += chunkSize){
+            chunks.add(Arrays.stream(Arrays.copyOfRange(tickers, i, Math.min(tickers.length, i + chunkSize))).collect(Collectors.toList()));
+        }
+
+        String url = "https://query1.finance.yahoo.com/v8/finance/spark?symbols=";
+        Map<String, Double> prices = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        for(List<String> subList : chunks) {
+            String tickersStr = String.join(",", subList);
+            try (CloseableHttpClient client = HttpClients.createMinimal()) {
+                try (CloseableHttpResponse response = client.execute(new HttpGet(url + tickersStr))) {
+                    String json = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+                    JsonNode node = mapper.readValue(json, JsonNode.class);
+                    Iterator<Map.Entry<String, JsonNode>> iter = node.fields();
+                    while (iter.hasNext()) {
+                        Map.Entry<String, JsonNode> entry = iter.next();
+                        if (entry.getValue().hasNonNull("previousClose")) {
+                            prices.put(entry.getKey(), entry.getValue().get("previousClose").asDouble());
+                        }
+                    }
+                }
+            } catch (IOException ignored) {}
+        }
+
+        return prices;
+    }
 }
 
