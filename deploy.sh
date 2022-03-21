fee365d60f1fc58657f5fdd56610e3bfc019b3e9
imckify <rokas.mockevicius@gmail.com>
2022-03-21 14:02:01 +0200

    deploy.sh update

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,9 @@ RewriteEngine On
 RewriteCond %{REQUEST_FILENAME} !-f
 RewriteRule ^ index.html [QSA,L]
 EOF
-#sshpass -p 'OR-studentui2203' scp -P 22265 -v -r target/* rokas@isk.ktu.lt:/var/www/l-s1/public_html/
+#  #old #sshpass -p 'OR-studentui2203' scp -P 22265 -v -r target/* rokas@isk.ktu.lt:/var/www/l-s1/public_html/
+
 sshpass -p 'OR-studentui2203' rsync -a -P -e "ssh -p 22265" /home/asd/IdeaProjects/Bakalauras/target/ rokas@isk.ktu.lt:/var/www/l-s1/public_html/
-sshpass -p 'OR-studentui2203' ssh rokas@isk.ktu.lt -p 22265 'pkill -9 -u rokas java && java -Dspring.profiles.active=prod -jar /var/www/l-s1/public_html/backend.jar' 
-echo Willass98 | sudo openconnect vpn.ktu.lt --user=rokmoc --passwd-on-stdin --usergroup=internet &
+#sshpass -p 'OR-studentui2203' ssh rokas@isk.ktu.lt -p 22265 'pkill -9 -u rokas java'
+sshpass -p 'OR-studentui2203' ssh rokas@isk.ktu.lt -p 22265 'pkill -15 -u rokas java && java -Dspring.profiles.active=prod -jar /var/www/l-s1/public_html/backend.jar'
+(echo Willass98 | sudo openconnect vpn.ktu.lt --user=rokmoc --passwd-on-stdin --usergroup=internet &)
